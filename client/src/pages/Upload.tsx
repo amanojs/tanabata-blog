8b7cc9024942f42ec69ce89f1fbd4fec605ddd4a
amanojs <takashivue07@gmail.com>
2020-07-19 19:00:03 +0900

    upload処理完成 1.0

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import axios from 'axios'
 
 export const Upload: React.FC = () => {
   const [files, setFiles] = React.useState<FileList | null>(null)
@@ -6,15 +7,30 @@ export const Upload: React.FC = () => {
     console.log(e.target.files)
     setFiles(e.target.files)
   }
-  const sendFile = () => {
+  const upFile = () => {
     console.log(files)
     const params = new FormData()
     params.append('file', files[0])
+    axios
+      .post('/api/upload', params, {
+        headers: { 'content-type': 'multipart/form-data' }
+      })
+      .then((result) => {
+        console.log(result)
+      })
+      .catch(() => {
+        console.log('upload failed')
+      })
   }
   return (
     <React.Fragment>
-      <input type="file" name="mdfile" onChange={(e) => selectFile(e)} />
-      <button onClick={() => sendFile()}>送信</button>
+      <input
+        type="file"
+        multiple
+        name="mdfile"
+        onChange={(e) => selectFile(e)}
+      />
+      <button onClick={() => upFile()}>送信</button>
     </React.Fragment>
   )
 }
