e67b81dc30b26715806fc3d15974542e23b181c3
amanojs <takashivue07@gmail.com>
2020-07-19 19:14:13 +0900

    ファイル保存処理追加 1.0

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 import express from 'express'
 import path from 'path'
+import multer from 'multer'
+const router = express.Router()
 
 const app = express()
-console.log(path.resolve('client/dist'))
 
 app.use(
   '/index.bundle.js',
   express.static(path.join('./', 'client', 'dist', 'index.bundle.js'))
 )
+
+const storage = multer.diskStorage({
+  // ファイルの保存先を指定
+  destination: function (req, file, cb) {
+    cb(null, '')
+  },
+  // ファイル名を指定(オリジナルのファイル名を指定)
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
+
+router.post('/api/upload/', upload.single('file'), (req, res) => {
+  res.json({ result: 'upload success.' })
+})
 
 const userRouter = require('./api/routers/userRouter')
 userRouter(app)
