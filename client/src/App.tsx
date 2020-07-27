c52eb544aa9849c28de02927e61d34527a6d52a0
takashiVue <takashivue@gmail.com>
2020-07-27 14:26:23 +0900

    dom lib追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,51 +13,54 @@ const App: React.FC = () => {
   const [menuFlag, setMenuFlag] = React.useState<boolean>(false)
   return (
     <React.Fragment>
-      <Router history={history}>
-        <AppBar position="static" style={{ backgroundColor: '#444' }}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={() => setMenuFlag(true)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <h6 onClick={() => history.push('/')} style={{ cursor: 'pointer' }}>
-              タナバタ.Blog
-            </h6>
-          </Toolbar>
-        </AppBar>
+      <React.Suspense fallback={<div>loading</div>}>
+        <Router history={history}>
+          <AppBar position="static" style={{ backgroundColor: '#444' }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={() => setMenuFlag(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <h6
+                onClick={() => history.push('/')}
+                style={{ cursor: 'pointer' }}
+              >
+                タナバタ.Blog
+              </h6>
+            </Toolbar>
+          </AppBar>
 
-        {menuFlag && <Menu setMenuFlag={setMenuFlag} />}
+          {menuFlag && <Menu setMenuFlag={setMenuFlag} />}
 
-        <Box bgcolor="#eee" width="100%" minHeight="100vh" padding="20px 0">
-          <React.Suspense fallback={<div>loading</div>}>
+          <Box bgcolor="#eee" width="100%" minHeight="100vh" padding="20px 0">
             <Switch>
               <Route exact path="/" component={Top} />
               <Route exact path="/genre" component={Top} />
               <Route exact path="/blog" component={BlogPage} />
             </Switch>
-          </React.Suspense>
-        </Box>
+          </Box>
 
-        <footer style={{ width: '100%' }}>
-          {/* <div style={{ height: '400px' }}></div> */}
-          <div
-            style={{
-              height: '60px',
-              backgroundColor: '#444',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              color: '#fff'
-            }}
-          >
-            <small>&copy;{new Date().getFullYear()} Nagoya Amano.js</small>
-          </div>
-        </footer>
-      </Router>
+          <footer style={{ width: '100%' }}>
+            {/* <div style={{ height: '400px' }}></div> */}
+            <div
+              style={{
+                height: '60px',
+                backgroundColor: '#444',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: '#fff'
+              }}
+            >
+              <small>&copy;{new Date().getFullYear()} Nagoya Amano.js</small>
+            </div>
+          </footer>
+        </Router>
+      </React.Suspense>
     </React.Fragment>
   )
 }
